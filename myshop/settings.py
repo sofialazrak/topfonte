fdc8ae86f05d2a46a18bb987ec12927eb6474e3e
sofialazrak <60613958+sofialazrak@users.noreply.github.com>
2021-03-10 17:30:12 +0100

    Update settings.py

--- myshop/settings.py
+++ myshop/settings.py
@@ -151,7 +151,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 CART_SESSION_ID = 'cart'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'sofia.lazrak@gmail.com'
-EMAIL_HOST_PASSWORD = 'Sol@med66'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
